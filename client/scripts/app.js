6c94ff54c46e893ea21c6211b5a3df6803ea6dbc
Eric King <e.king117@gmail.com>
2015-07-13 20:10:42 -0400

    can now post messages to the 'test' board

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,25 +20,67 @@
 //   }
 // });
 
+$(document).ready(function(){
 
-var getMessages = function(){
-  return $.ajax({
-    url: "https://api.parse.com/1/classes/chatterbox",
-    type: 'GET',
-    contentType: 'application/json',
-    success: function(data) {
-      $('.messages').empty();
-      console.log('Request Made!!');
-      for (var i = 0; i < data.results.length; i++) {
-        $('.messages').append("<div class='message'>" + _.escape(data.results[i].text) + "</div>");
+  var getMessages = function(){
+    return $.ajax({
+      url: "https://api.parse.com/1/classes/chatterbox",
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log(data);
+        $('.messages').empty();
+        console.log('Request Made!!');
+        for (var i = 0; i < data.results.length; i++) {
+          $('.messages').append("<div class='message'>" + _.escape(data.results[i].text) + "</div>");
+        }
+      },
+
+      error: function(data) {
+        console.log(data);
       }
-    },
+    });
+  };
 
-    error: function(data) {
-      console.log(data);
-    }
+  var postMessage = function(message) {
+    $.ajax({
+      url: "https://api.parse.com/1/classes/chatterbox",
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(data) {
+        console.log(data);
+      }
+    });
+  };
+
+  getMessages();
+  // setInterval(getMessages, 15000);
+
+  // SELECT USER NAME AND SEND MESSAGE
+
+
+  $('.button').on('click', function(e){
+    var message = {};
+    message.text = $('.userInput').val();
+    message.username = window.decodeURIComponent(window.location.search).split('=')[1];
+    message.roomname = 'test';
+
+    postMessage(message);
   });
-};
 
-getMessages();
-setInterval(getMessages, 3000);
+});
+
+// need a data (form) field, with a submit button
+
+// put request in a function to invoke later (ie click)
+  // function will:
+  // get user name
+  // get text
+  // get room name (?)
+  // format above data, make POST request
+  // console.log code
+
